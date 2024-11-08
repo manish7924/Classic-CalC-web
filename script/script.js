d457f779ce6c6c4cc272f490939b84a65b67ec12
Manish Chidar <114106502+manish7924@users.noreply.github.com>
2024-11-08 06:27:23 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,27 +1,30 @@
 let input = document.getElementById('inputbox')
-let button =document.querySelectorAll('button')
+let button = document.querySelectorAll('button')
 
 let string = "";
 let arr = Array.from(button)
-arr.forEach(button =>{
-    button.addEventListener('click',(e)=>{
-        if(e.target.innerHTML =='='){
-            string= eval(string);
-            input.value=string;
+arr.forEach(button => {
+    button.addEventListener('click',(e) => {
+        if(e.target.innerHTML == '=') {
+            string = eval(string);
+            input.value = string;
         }
-        else if(e.target.innerHTML=='AC' ){
-            string="";
-            input.value= string;
+        else if(e.target.innerHTML == 'AC' ){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == '%'){
+                string = string.substring(0, string.length-1);
+                input.value = string;
         }
         else if
-            (e.target.innerHTML=='DEL'){
-                string = string.substring(0,string. length-1);
-                input.value =string;
-            }
-        
-        else{
+            (e.target.innerHTML == 'DEL'){
+                string = string.substring(0, string.length-1);
+                input.value = string;
+        }
+        else {
             string += e.target.innerHTML;
-            input.value= string;
+            input.value = string;
         }
     })
-})
+}
